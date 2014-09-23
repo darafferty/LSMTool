01e646f2188f1f49c6c7e840b883a7934c95d621
David Rafferty <drafferty@hs.uni-hamburg.de>
2014-09-23 14:44:10 +0200

    Added asserts to tests

--- tests/test_lsmtool.py
+++ tests/test_lsmtool.py
@@ -1,39 +1,80 @@
 #! /usr/bin/env python
 # Runs an example of each operation
 import lsmtool
+import os
+
 
 s = lsmtool.load('tests/no_patches.sky')
 
-print('Select individual sources with Stokes I fluxes above 1 Jy')
-s.select('I > 1.0 Jy')
 
-print('Transfer patches from patches.sky')
-s.transfer('tests/patches.sky')
+def test_select():
+    print('Select individual sources with Stokes I fluxes above 1 Jy')
+    s.select('I > 1.0 Jy')
+    assert len(s) == 965
 
-print('Remove patches with total fluxes below 2 Jy')
-s.remove('I < 2.0 Jy', aggregate='sum')
 
-print('Ungroup the skymodel')
-s.ungroup()
+def test_transfer():
+    print('Transfer patches from patches.sky')
+    s.transfer('tests/patches.sky')
+    assert s.hasPatches
 
-print('Concatenate with concat.sky')
-s.concatenate('tests/concat.sky', matchBy = 'position', radius = '30 arcsec', keep = 'from2')
 
-print('Add a source')
-s.add({'Name': 'src1', 'Type': 'POINT', 'Ra': 277.4232, 'Dec': 48.3689, 'I': 0.69})
+def test_remove():
+    print('Remove patches with total fluxes below 2 Jy')
+    s.remove('I < 2.0 Jy', aggregate='sum')
+    assert len(s) == 433
 
-print('Group using tessellation to a target flux of 50 Jy')
-s.group('tessellate', targetFlux = '50.0 Jy')
 
-print('Move patch Patch_1 to 16:04:16.2288, 58.03.06.912')
-s.move('Patch_1', position =  ['16:04:16.2288', '58.03.06.912'])
+def test_upgroup():
+    print('Ungroup the skymodel')
+    s.ungroup()
+    assert ~s.hasPatches
 
-print('Merge patches Patch_0 and Patch_2')
-s.merge(['Patch_0', 'Patch_2'], name = 'merged_patch')
 
-print('Set patch positions to midpoint of patch and write final model to file')
-s.setPatchPositions(method='mid')
-s.write('tests/final.sky', clobber=True)
+def test_concatenate():
+    print('Concatenate with concat.sky')
+    s.concatenate('tests/concat.sky', matchBy = 'position', radius = '30 arcsec', keep = 'from2')
+    assert len(s) == 2165
 
-print('Plot the sky model')
-s.plot('tests/plot.pdf')
+
+def test_add():
+    print('Add a source')
+    s.add({'Name': 'src1', 'Type': 'POINT', 'Ra': 277.4232, 'Dec': 48.3689, 'I': 0.69})
+    assert len(s) == 2166
+
+
+def test_group():
+    print('Group using tessellation to a target flux of 50 Jy')
+    s.group('tessellate', targetFlux = '50.0 Jy')
+    assert s.hasPatches
+
+
+def test_move():
+    print('Move patch Patch_1 to 16:04:16.2288, 58.03.06.912')
+    s.move('Patch_1', position =  ['16:04:16.2288', '58.03.06.912'])
+    assert s.getPatchPositions()['Patch_1'][0].value == 241.06762
+
+
+def test_merge():
+    print('Merge patches Patch_0 and Patch_2')
+    s.merge(['Patch_0', 'Patch_2'], name = 'merged_patch')
+    assert len(s.getPatchNames()) == 102
+
+
+def test_setPatchPositions():
+    print('Set patch positions to midpoint of patch')
+    s.setPatchPositions(method='mid')
+    assert s.getPatchPositions()['merged_patch'][0].value == 277.76634967991293
+
+
+def test_write():
+    print('Write final model to file')
+    s.write('tests/final.sky', clobber=True)
+    assert os.path.exists('tests/final.sky')
+
+
+def test_plot():
+    print('Plot the model')
+    s.plot('tests/plot.pdf')
+    assert os.path.exists('tests/plot.pdf')
+
