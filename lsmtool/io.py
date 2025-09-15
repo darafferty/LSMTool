1649f1193517b8ebea4febd52cf462d24abc6b17
astromancer <hannes.breytenbach@skao.int>
2025-09-15 15:58:34 +0200

    GEC-161: Remove array padding

--- lsmtool/io.py
+++ lsmtool/io.py
@@ -270,9 +270,10 @@ def read_vertices(filename, wcs):
     # Convert to image pixel coordinates (x, y). Note: we need to add two extra
     # (dummy) elements to the celestial coordinates, since the wcs has four
     # axes, and then remove them from the resulting image coordinates.
-    vertices_image = wcs.wcs_world2pix(
-        np.pad(vertices_celestial, [(0, 0), (0, 2)]), WCS_ORIGIN
-    )[:, 0:2]
+    null_coordinates = [0] * (wcs.naxis - 2)
+    *vertices_image, _, _ = wcs.wcs_world2pix(
+        *vertices_celestial.T, *null_coordinates, WCS_ORIGIN
+    )
 
     # Convert to a list of (x, y) tuples.
     return list(zip(*vertices_image))
