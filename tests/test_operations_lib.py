b213f613f18dd0d548618c7c23664de91d6e50a9
astromancer <hannes.breytenbach@skao.int>
2025-09-29 13:41:41 +0200

    GEC-115: Parameterize test

--- tests/test_operations_lib.py
+++ tests/test_operations_lib.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import filecmp
-import numpy as np
 import pathlib
-import requests
 import tarfile
 import tempfile
 import unittest
 
+import numpy as np
+import pytest
+import requests
 from numpy.testing import assert_array_equal
 
 import lsmtool
@@ -149,71 +150,97 @@ def test_make_wcs_custom():
     assert_array_equal(w.wcs.ctype, ["RA---TAN", "DEC--TAN"])
 
 
-def test_tessellate():
+@pytest.mark.parametrize(
+    "ra_cal, ra_dec, ra_mid, dec_mid, width_ra, width_dec, "
+    "expected_facet_points, expected_facet_polygons",
+    [
+        pytest.param(
+            RA_CAL := [119.73, 138.08, 124.13, 115.74],
+            DEC_CAL := [89.92, 89.91, 89.89, 89.89],
+            126.52,
+            90.0,
+            0.3,
+            0.3,
+            # expected_facet_points
+            np.transpose([RA_CAL, DEC_CAL]),
+            #
+            [
+                [
+                    [127.3, 89.9],
+                    [278.9, 89.8],
+                    [351.5, 89.8],
+                    [51.9, 89.8],
+                    [119.9, 89.9],
+                    [127.3, 89.9],
+                ],
+                [
+                    [127.3, 89.9],
+                    [278.9, 89.8],
+                    [261.5, 89.8],
+                    [171.5, 89.8],
+                    [146.9, 89.8],
+                    [127.3, 89.9],
+                ],
+                [
+                    [127.3, 89.9],
+                    [146.9, 89.8],
+                    [119.9, 89.8],
+                    [119.9, 89.9],
+                    [127.3, 89.9],
+                ],
+                [
+                    [119.9, 89.9],
+                    [51.9, 89.8],
+                    [81.5, 89.8],
+                    [119.9, 89.8],
+                    [119.9, 89.9],
+                ],
+            ],
+        )
+    ],
+)
+def test_tessellate(
+    ra_cal,
+    ra_dec,
+    ra_mid,
+    dec_mid,
+    width_ra,
+    width_dec,
+    expected_facet_points,
+    expected_facet_polygons,
+):
     """
-    Test the tessellate function, using a region that encompasses the North 
+    Test the tessellate function, using a region that encompasses the North
     Celestial Pole (NCP).
     """
 
     # Test a region that encompasses the NCP.
     facet_points, facet_polys = tessellate(
-        RA_CAL := [119.73, 138.08, 124.13, 115.74],
-        DEC_CAL := [89.92, 89.91, 89.89, 89.89],
-        126.52,
-        90.0,
-        0.3,
-        0.3,
+        ra_cal,
+        ra_dec,
+        ra_mid,
+        dec_mid,
+        width_ra,
+        width_dec,
     )
 
     # Check the facet points
-    np.testing.assert_allclose(facet_points, np.transpose([RA_CAL, DEC_CAL]))
+    np.testing.assert_allclose(facet_points, expected_facet_points)
 
-    # Check the facet polygons. Since the start point of each polygon can
-    # be different from the control but the polygons still be identical,
-    # we check only that each vertex is present in the control (and
+    # Check the facet polygons. Since the starting point of each polygon can be
+    # different from the expected array, with the polygons still being
+    # identical, we check only that each vertex is present in the control (and
     # vice versa)
     facet_polys = [np.round(a, 1).tolist() for a in facet_polys]
-    facet_polys_control = [
-        [
-            [127.3, 89.9],
-            [278.9, 89.8],
-            [351.5, 89.8],
-            [51.9, 89.8],
-            [119.9, 89.9],
-            [127.3, 89.9],
-        ],
-        [
-            [127.3, 89.9],
-            [278.9, 89.8],
-            [261.5, 89.8],
-            [171.5, 89.8],
-            [146.9, 89.8],
-            [127.3, 89.9],
-        ],
-        [
-            [127.3, 89.9],
-            [146.9, 89.8],
-            [119.9, 89.8],
-            [119.9, 89.9],
-            [127.3, 89.9],
-        ],
-        [
-            [119.9, 89.9],
-            [51.9, 89.8],
-            [81.5, 89.8],
-            [119.9, 89.8],
-            [119.9, 89.9],
-        ],
-    ]
+    facet_polys_flat = _flatten(facet_polys)
 
-    facet_polys_flat = [tuple(point) for poly in facet_polys for point in poly]
-    facet_polys_control_flat = [
-        tuple(point) for poly in facet_polys_control for point in poly
-    ]
-    difference = set(facet_polys_flat).symmetric_difference(
-        facet_polys_control_flat
-    )
+    facet_polys_control_flat = _flatten(expected_facet_polygons)
+    difference = facet_polys_flat.symmetric_difference(facet_polys_control_flat)
     assert not difference
+
+
+def _flatten(iterable):
+    return {tuple(point) for item in iterable for point in item}
 
 
 if __name__ == "__main__":
