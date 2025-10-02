a297ed02e9000da46998ca94763dee3cbb334be6
astromancer <hannes.breytenbach@skao.int>
2025-10-02 12:56:50 +0200

    GEC-115: Introduce `facet` module

--- tests/test_operations_lib.py
+++ tests/test_operations_lib.py
@@ -7,7 +7,6 @@ import tempfile
 import unittest
 
 import numpy as np
-import pytest
 import requests
 from numpy.testing import assert_array_equal
 
@@ -15,8 +14,7 @@ import lsmtool
 from lsmtool.operations_lib import (
     apply_beam,
     make_wcs,
-    normalize_ra_dec,
-    tessellate,
+    normalize_ra_dec
 )
 
 
@@ -101,11 +99,7 @@ class TestOperationsLib(unittest.TestCase):
         outfile = str(self.temp_path / "test_apply_beam_invert.out")
         reffile = str(self.resource_path / "test_apply_beam_invert.out")
         result = apply_beam(
-            str(self.ms_path),
-            self.flux,
-            self.ra,
-            self.dec,
-            invert=True
+            str(self.ms_path), self.flux, self.ra, self.dec, invert=True
         )
         np.set_printoptions(precision=6)
         with open(outfile, "w") as f:
@@ -119,7 +113,7 @@ class TestOperationsLib(unittest.TestCase):
         ra = 450.0
         dec = 95.0
         result = normalize_ra_dec(ra, dec)
-        assert (result.ra == 270.0 and result.dec == 85.0)
+        assert result.ra == 270.0 and result.dec == 85.0
 
 
 def test_make_wcs_default():
@@ -148,99 +142,6 @@ def test_make_wcs_custom():
     assert_array_equal(w.wcs.cdelt, [-crdelt, crdelt])
     assert_array_equal(w.wcs.crval, [ref_ra, ref_dec])
     assert_array_equal(w.wcs.ctype, ["RA---TAN", "DEC--TAN"])
-
-
-@pytest.mark.parametrize(
-    "ra_cal, ra_dec, ra_mid, dec_mid, width_ra, width_dec, "
-    "expected_facet_points, expected_facet_polygons",
-    [
-        pytest.param(
-            RA_CAL := [119.73, 138.08, 124.13, 115.74],
-            DEC_CAL := [89.92, 89.91, 89.89, 89.89],
-            126.52,
-            90.0,
-            0.3,
-            0.3,
-            # expected_facet_points
-            np.transpose([RA_CAL, DEC_CAL]),
-            #
-            [
-                [
-                    [127.3, 89.9],
-                    [278.9, 89.8],
-                    [351.5, 89.8],
-                    [51.9, 89.8],
-                    [119.9, 89.9],
-                    [127.3, 89.9],
-                ],
-                [
-                    [127.3, 89.9],
-                    [278.9, 89.8],
-                    [261.5, 89.8],
-                    [171.5, 89.8],
-                    [146.9, 89.8],
-                    [127.3, 89.9],
-                ],
-                [
-                    [127.3, 89.9],
-                    [146.9, 89.8],
-                    [119.9, 89.8],
-                    [119.9, 89.9],
-                    [127.3, 89.9],
-                ],
-                [
-                    [119.9, 89.9],
-                    [51.9, 89.8],
-                    [81.5, 89.8],
-                    [119.9, 89.8],
-                    [119.9, 89.9],
-                ],
-            ],
-        )
-    ],
-)
-def test_tessellate(
-    ra_cal,
-    ra_dec,
-    ra_mid,
-    dec_mid,
-    width_ra,
-    width_dec,
-    expected_facet_points,
-    expected_facet_polygons,
-):
-    """
-    Test the tessellate function, using a region that encompasses the North
-    Celestial Pole (NCP).
-    """
-
-    # Tessellate a region that encompasses the NCP.
-    facet_points, facet_polys = tessellate(
-        ra_cal,
-        ra_dec,
-        ra_mid,
-        dec_mid,
-        width_ra,
-        width_dec,
-    )
-
-    # Check the facet points
-    np.testing.assert_allclose(facet_points, expected_facet_points)
-
-    # Check the facet polygons. Since the tessellate function is not
-    # guaranteed to return the same order of vertices for identical
-    # polygons, check only that the set of expected and actual vertices
-    # are identical.
-    facet_polys = [np.round(a, 1).tolist() for a in facet_polys]
-    facet_polys_flat = _flatten(facet_polys)
-
-    facet_polys_expected = _flatten(expected_facet_polygons)
-    difference = facet_polys_flat.symmetric_difference(facet_polys_expected)
-    assert not difference
-
-
-def _flatten(iterable):
-    return {tuple(point) for item in iterable for point in item}
 
 
 if __name__ == "__main__":
