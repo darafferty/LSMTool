ebf2376854668f2a709fc8f2ff2400fcbd959a98
astromancer <hannes.breytenbach@skao.int>
2026-04-20 11:22:27 +0200

    GEC-115: Update default pixel scale

--- lsmtool/constants.py
+++ lsmtool/constants.py
@@ -5,4 +5,4 @@ Module containing constant values (defaults) used throughout the package.
 # Always use a 0-based origin in wcs_pix2world and wcs_world2pix calls.
 WCS_ORIGIN = 0
 
-WCS_PIXEL_SCALE = 20.0 / 3600.0  # 20"/pixel
+WCS_PIXEL_SCALE = 10.0 / 3600.0  # 10"/pixel
