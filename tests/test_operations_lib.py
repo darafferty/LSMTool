2befa6236a6f6f6c14e4e722340fbf0b14c08f43
astromancer <hannes.breytenbach@skao.int>
2026-05-07 22:35:35 +0200

    Convert old style unit tests to pytest

--- tests/test_operations_lib.py
+++ tests/test_operations_lib.py
@@ -1,109 +1,36 @@
-#!/usr/bin/env python3
+"""
+Tests for the `lsmtool.operations_lib` module.
+"""
 
 import filecmp
-import requests
-import tarfile
-import tempfile
-import unittest
-from pathlib import Path
 
 import numpy as np
 import pytest
 from numpy.testing import assert_allclose, assert_array_equal
 
-import lsmtool
 from lsmtool.operations_lib import apply_beam, make_wcs, normalize_ra_dec
 
 
-class TestOperationsLib(unittest.TestCase):
-    """
-    Test class for the module `operations_lib`
+@pytest.mark.parametrize("invert", [False, True])
+def test_apply_beam(
+    tmp_path, test_data_path, test_ms_lofar_hba, lofar_hba_skymodel, invert
+):
+    """Test `apply_beam` function"""
+    filename = f"test_apply_beam{'_invert' * invert}.out"
+    output_path = tmp_path / filename
+    reference_path = test_data_path / filename
 
-    For testing a Measurement Set will be downloaded, if not present in the
-    `tests/resources` directory. The skymodel was generated, using the pointing
-    information in this MS, using the following commands:
-    ```
-    s = lsmtool.load('TGSS', VOPosition=["08:13:36.067800", "+48.13.02.58100"],
-                     VORadius=1.0)
-    s.write('tests/resources/LOFAR_HBA_MOCK.sky')
-    ```
-    """
+    result = apply_beam(
+        str(test_ms_lofar_hba),
+        lofar_hba_skymodel.getColValues("I"),
+        lofar_hba_skymodel.getColValues("RA"),
+        lofar_hba_skymodel.getColValues("Dec"),
+        invert=invert,
+    )
+    np.set_printoptions(precision=6)
+    output_path.write_text(str(result))
 
-    @classmethod
-    def setUpClass(cls):
-        """
-        Download and unpack Measurement Set used for testing
-        """
-        # Path to directory containing test resources
-        cls.resource_path = Path(__file__).parent / "resources"
-
-        # Path to the input Measurement Set (will be downloaded if non-existent)
-        cls.ms_path = cls.resource_path / "LOFAR_HBA_MOCK.ms"
-
-        # URL to download location of input Measurement Set
-        cls.ms_url = "https://support.astron.nl/software/ci_data/EveryBeam/L258627-one-timestep.tar.bz2"
-
-        # Path to the input Sky Model (will be created if non-existent)
-        cls.skymodel_path = cls.resource_path / "LOFAR_HBA_MOCK.sky"
-
-        # Create a temporary test directory
-        cls.temp_dir = tempfile.TemporaryDirectory()
-        cls.temp_path = Path(cls.temp_dir.name)
-
-        # Download MS if it's not available in resource directory
-        if not cls.ms_path.exists():
-            cls.download_ms()
-
-    @classmethod
-    def download_ms(cls):
-        """
-        Download and unpack MS in our temporary test directory
-        """
-        # Filter to strip leading component from file names
-        # (like `tar --strip-components=1`)
-        filter_ = lambda member, path: member.replace(
-            name=Path(*Path(member.path).parts[1:])
-        )
-        with requests.get(cls.ms_url, stream=True) as req:
-            with tarfile.open(fileobj=req.raw, mode="r|bz2") as tarobj:
-                tarobj.extractall(path=cls.ms_path, filter=filter_)
-
-    def setUp(self):
-        self.skymodel = lsmtool.load(str(self.skymodel_path))
-        self.ra = self.skymodel.getColValues("RA")
-        self.dec = self.skymodel.getColValues("Dec")
-        self.flux = self.skymodel.getColValues("I")
-
-    def test_apply_beam(self):
-        """
-        Test `apply_beam` function
-        """
-        outfile = str(self.temp_path / "test_apply_beam.out")
-        reffile = str(self.resource_path / "test_apply_beam.out")
-        result = apply_beam(
-            str(self.ms_path),
-            self.flux,
-            self.ra,
-            self.dec,
-        )
-        np.set_printoptions(precision=6)
-        with open(outfile, "w") as f:
-            f.write(str(result))
-        assert filecmp.cmp(reffile, outfile, shallow=False)
-
-    def test_apply_beam_invert(self):
-        """
-        Test `apply_beam` function with inverted beam
-        """
-        outfile = str(self.temp_path / "test_apply_beam_invert.out")
-        reffile = str(self.resource_path / "test_apply_beam_invert.out")
-        result = apply_beam(
-            str(self.ms_path), self.flux, self.ra, self.dec, invert=True
-        )
-        np.set_printoptions(precision=6)
-        with open(outfile, "w") as f:
-            f.write(str(result))
-        assert filecmp.cmp(reffile, outfile, shallow=False)
+    assert filecmp.cmp(reference_path, output_path, shallow=False)
 
 
 @pytest.mark.parametrize(
@@ -144,7 +71,3 @@ def test_make_wcs_custom():
     assert_array_equal(w.wcs.cdelt, [-crdelt, crdelt])
     assert_array_equal(w.wcs.crval, [ref_ra, ref_dec])
     assert_array_equal(w.wcs.ctype, ["RA---TAN", "DEC--TAN"])
-
-
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
