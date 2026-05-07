bf05c35c568783ca9faf4365f2152485502c18ad
Vijay Mahatma <vm462@cam.ac.uk>
2026-05-07 16:22:04 +0000

    GEC-462: add skymodelreader tests

---
+++ tests/test_tableio.py
@@ -0,0 +1,29 @@
+from astropy.table import Table
+
+from lsmtool.skymodel import skyModelReader
+
+def test_skymodelreader_emptyfile(tmp_path):
+    """ Test skyModelReader returns empty table for empty file """
+
+    # Create a fake existing sky model file to test functionality
+    skymodel_path = tmp_path/ "empty.sky"
+    skymodel_path.touch()
+
+    table = skyModelReader(str(skymodel_path))
+
+    assert isinstance(table,Table)
+    assert(len(table)) == 0
+
+def test_skymodelreader_headeronly(tmp_path):
+    """ Test skyModelReader returns empty table for header-only file """
+
+    # Create a fake existing sky model file to test functionality
+    skymodel_path = tmp_path/ "empty.sky"
+    skymodel_path.write_test(
+        "FORMAT = Name, Type, Ra, Dec, I\n"
+    )
+
+    table = skyModelReader(str(skymodel_path))
+
+    assert isinstance(table,Table)
+    assert len(table) == 0
