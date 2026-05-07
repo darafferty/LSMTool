4e5cf93046e309844c04382375f86e68929bd2d8
Vijay Mahatma <vm462@cam.ac.uk>
2026-05-07 17:36:11 +0000

    GEC-462: change test to raise IOError

--- tests/test_tableio.py
+++ tests/test_tableio.py
@@ -1,18 +1,17 @@
 from astropy.table import Table
+import pytest
 
 from lsmtool.tableio import skyModelReader
 
 def test_skymodelreader_emptyfile(tmp_path):
-    """ Test skyModelReader returns empty table for empty file """
+    """ Test skyModelReader raises IOError for empty file """
 
     # Create a fake existing sky model file to test functionality
     skymodel_path = tmp_path/ "empty.sky"
     skymodel_path.touch()
 
-    table = skyModelReader(str(skymodel_path))
-
-    assert isinstance(table,Table)
-    assert(len(table)) == 0
+    with pytest.raises(IOError):
+        skyModelReader(str(skymodel_path))
 
 def test_skymodelreader_headeronly(tmp_path):
     """ Test skyModelReader returns empty table for header-only file """
